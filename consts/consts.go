1caaf18c57fb66ea2f4b7bb7849122a8607b9989
Aayush Tewari <113117171+Aayushow1912@users.noreply.github.com>
2024-09-26 23:27:56 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "AAYU"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "AAYUSH"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "TEWARI"
 )
 
 var ID ids.ID
